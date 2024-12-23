e252198e791b941594906ad201ec114add9a6108
hammadshakeelai <hammadshakeel61@gmail.com>
2024-12-24 02:15:49 +0500

    dope

--- 100 Day Challenge/Day_11.py
+++ 100 Day Challenge/Day_11.py
@@ -1,6 +1,6 @@
 #Objective make a random name selector
 num=2
-if num%2==0:
+if num==1:
     names = input("Enter the csv name text: ")
     name_list = names.split(",")
 
@@ -8,6 +8,13 @@ if num%2==0:
 
     person_paying = random.choice(name_list)
     print("The person to get their wallet empty is.....", person_paying)
+elif num==2:
+    names=input("enter the comma seperated names as text:  ")
+    name_list=names.split(",")
+    number_of_name=len(name_list)
+    import random
+    index_selected=random.randint(0,number_of_name-1)
+    print(f"name that got choosen is {name_list[index_selected].strip()}.")
 else:
     number_of_players=int(input("how many palyers are there: "))
     names_of_people=''
---
+++ 100 Day Challenge/Day_12.py
@@ -0,0 +1,4 @@
+my_list=["me","you","he","him"]
+my_list.append("her")#use append to an a new item to the end of the string
+my_list.extend(["his","she","mine"])#use extend to extend the list with item from this list in extends input parameter
+print(my_list)
