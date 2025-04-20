b6825833eadcdc9dbec84e4adbfc0f1b7aad01f5
hammadshakeelai <hammadshakeel61@gmail.com>
2025-04-20 23:37:43 +0500

    oo

--- 100 Day Challenge/Day_15.py
+++ 100 Day Challenge/Day_15.py
@@ -9,7 +9,7 @@ print(f'sum of all even numbers till {n_num} is : {answer}')
 import random
 password=''
 my_list = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '[', ']', '|', '}', '{', '>', '<', '?', '/', '.', ',', ';', ':', '}','1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '`', '"', '1','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-password_length = 13
+password_length = input("enter length of password you want generated: ")
 for _ in range( password_length ) :
     password += random.choice( my_list )
 print(f'This Your Generated Password : {password}')
